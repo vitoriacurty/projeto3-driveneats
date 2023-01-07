bd38d1aff231fe3218ec0378d5983603382a13f5
vitoriacurty <vitoria.curty@hotmail.com>
2023-01-07 19:44:40 -0300

    implementado funcionalidade de enviar pedido no whatsapp

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,12 +25,37 @@ function getSobremesa(sobremesa) {
   activeButton()
 }
 
-  function activeButton(){
-    let quantidade = document.getElementsByClassName("green-border").length
-    if(quantidade === 3){
-        document.querySelector("button").classList.add("botao-enviar")
-        document.querySelector("button p").classList.add("fechar-pedido")
-        document.querySelector("button p").innerHTML = "Fechar pedido"
-        document.querySelector("button").removeAttribute("disabled")
-    }
+function activeButton() {
+  let quantidade = document.getElementsByClassName('green-border').length
+  if (quantidade === 3) {
+    document.querySelector('button').classList.add('botao-enviar')
+    document.querySelector('button p').classList.add('fechar-pedido')
+    document.querySelector('button p').innerHTML = 'Fechar pedido'
+    document.querySelector('button').removeAttribute('disabled')
+  }
+}
+
+function enviarPedido() {
+  const prato = document.querySelector("#pratos-principais .green-border h4").innerHTML;
+  const bebida = document.querySelector("#bebidas .green-border h4").innerHTML;
+  const sobremesa = document.querySelector("#sobremesas .green-border h4").innerHTML;
+
+  const precoPrato = document.querySelector("#pratos-principais .green-border .valor").innerHTML;
+  const precoBebida = document.querySelector("#bebidas .green-border .valor").innerHTML;
+  const precoSobremesa = document.querySelector("#sobremesas .green-border .valor").innerHTML;
+
+  const precoPratoNumber = valores(precoPrato);
+  const precoBebidaNumber = valores(precoBebida);
+  const precoSobremesaNumber = valores(precoSobremesa);
+  const valorTotal = precoPratoNumber + precoBebidaNumber + precoSobremesaNumber;
+
+  const mensagem = `Olá, gostaria de fazer o pedido: \n- Prato: ${prato} \n- Bebida: ${bebida} \n- Sobremesa: ${sobremesa} \n- Total: R$ ${valorTotal.toFixed(2)}`
+
+  const whatsapp = "https://wa.me/5524999523474?text=" + encodeURI(mensagem);
+  window.open(whatsapp);
+}
+
+function valores(string) {
+  let numero = string.replace(/[^0-9]/g, "");
+  return parseInt(numero) / 100;
 }
